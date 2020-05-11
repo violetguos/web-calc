a4097c3e3a50ebfc31a1d75cc0e218ea927122a3
Y. Violet Guo <yueguo598@gmail.com>
2020-05-11 11:14:27 -0300

    Can click and respond 7 8 9

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// math functions
 function add(a, b) {
 	return a + b;	
 }
@@ -18,3 +19,21 @@ function operate(cmd, a, b){
     // use cmd directly as a func
     cmd(a, b);
 }
+
+// button controls
+function numberButtons(){
+    let a, b;
+    const btns = document.querySelectorAll(".number");
+    btns.forEach(
+        function (btn){
+            btn.addEventListener('click', function() {
+                // get a and b
+                console.log(btn.textContent);
+                a = Number(btn.textContent);
+            }
+            )
+           
+        }
+    )
+}
+numberButtons();
