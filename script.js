24119ec277ceae7df0b8d86da772f6edf7bf1463
Y. Violet Guo <yueguo598@gmail.com>
2020-05-11 12:06:15 -0300

    only integer addition

--- script.js
+++ script.js
@@ -17,23 +17,62 @@ function divide(a, b){
 
 function operate(cmd, a, b){
     // use cmd directly as a func
-    cmd(a, b);
+    return cmd(a, b);
+}
+
+function arrayPopEval(){
+    // pop the global array elements
+    // only integers for now
+    let a = 0;
+    let b = 0;
+    let cmd;
+    while(arrayButtons.length > 0){
+        // since we pop, fill b first
+        let btn = arrayButtons.pop();
+        if(b === 0)
+            b = Number(btn);
+        else if(btn === '+')
+            cmd = add;
+        else if(a === 0);
+            a = Number(btn);
+    }
+    console.log("cmd", cmd);
+    let result = operate(cmd, a, b);
+    console.log(result);
 }
 
 // button controls
 function numberButtons(){
-    let a, b;
-    const btns = document.querySelectorAll(".number");
+    // record array of numbers pressed
+    // query all the buttons
+    const btns = document.querySelectorAll("button");
     btns.forEach(
         function (btn){
             btn.addEventListener('click', function() {
                 // get a and b
                 console.log(btn.textContent);
-                a = Number(btn.textContent);
-            }
+                // TODO: check whether CLEAR or EQUAL is pressed
+                if(btn.textContent === "="){
+                    arrayPopEval();
+                }
+                else if(btn.textContent === "AC"){
+                    // clear everything
+                    console.log("before", arrayButtons);
+                    arrayButtons = [];
+                    console.log(arrayButtons);
+                }
+                else{
+                    // keep pressing numbers 
+                    arrayButtons.push(btn.textContent);
+                }
+                }
             )
            
         }
     )
 }
+
+// global operands
+let operation; // undefined;
+let arrayButtons = []; // length == 0 when empty
 numberButtons();
